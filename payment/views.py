b36714c03f0541cba0cf444bb2efc32cceba40f5
pouya007RN <35175878+pouya007RN@users.noreply.github.com>
2019-11-13 20:46:41 +0300

    Add files via upload

---
+++ payment/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Github.com/Rasooll
+from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404, render
+from zeep import Client
+from orders.models import Order
+
+
+
+
+'''MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
+  # Toman / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = 'email@example.com'  # Optional
+mobile = '09123456789'  # Optional
+CallbackURL = 'http://localhost:8000/verify/' # Important: need to edit for realy server.
+
+def send_request(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    amount = int(order.get_total_price())
+    result = client.service.PaymentRequest(MERCHANT, amount, description, email, mobile, CallbackURL)
+    if result.Status == 100:
+        return redirect('https://www.zarinpal.com/pg/StartPay/' + str(result.Authority))
+    else:
+        return render(request, 'payment/badrequest.html',{'Error':'Error code: ' + str(result.Status)})
+
+def verify(request):
+    if request.GET.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
+        if result.Status == 100:
+            return HttpResponse('Transaction success.\nRefID: ' + str(result.RefID))
+        elif result.Status == 101:
+            return HttpResponse('Transaction submitted : ' + str(result.Status))
+        else:
+            return HttpResponse('Transaction failed.\nStatus: ' + str(result.Status))
+    else:
+        return HttpResponse('Transaction failed or canceled by user')'''
